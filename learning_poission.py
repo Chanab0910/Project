d1a05631de20340903907eb0bbb98f4ce353f782
chanab0910 <chanab0910@highgateschool.org.uk>
2023-06-23 09:14:23 +0100

    Prototype done

--- learning_poission.py
+++ learning_poission.py
@@ -1,7 +1,20 @@
 from numpy import random
 
-var = 0.0128 * 90
+base = 0.0128125
+Germany_attack = 72
+Germany_defense = 46
+Sweden_attack = 64
+Sweden_defense = 40
 
-for i in range(10):
-    x = random.poisson(lam=var)
-    print(x)
+
+def Calculate_Goals(attack, defense):
+    changed_minute_base = base * attack / defense
+    ninety_min = changed_minute_base * 90
+    num_goals = random.poisson(ninety_min)
+    return num_goals
+
+
+Gr_goals = Calculate_Goals(Germany_attack, Sweden_defense)
+Sw_goals = Calculate_Goals(Sweden_attack, Germany_defense)
+
+print(Gr_goals, '-', Sw_goals)
